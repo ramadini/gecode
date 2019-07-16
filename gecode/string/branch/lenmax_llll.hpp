c33952e695b655326289add7253f8b2892f2df0e
robama <amadini@cs.unibo.it>
2019-07-16 17:12:39 +1000

    Fixed commit methods.

--- gecode/string/branch/lenmax_llll.hpp
+++ gecode/string/branch/lenmax_llll.hpp
@@ -37,29 +37,37 @@ namespace Gecode { namespace String { namespace Branch {
 
     ExecStatus
     LenMax_LLLL::commit(Space& home, const Choice& c, unsigned a) {
-        const PosLevVal& p = static_cast<const PosLevVal&>(c);
-        const std::vector<string>& ext = x[p.pos].ext_list();
-        if (ext.empty()) {
-          x[p.pos].commit(home, p.lev, p.val, Block::FIRST, a);
-          return ES_OK;
-        }
-  //      std::cerr<<"ext list = ["; for(auto x:ext) std::cerr<<"\""<<x<< "\", "; std::cerr<<"]\n";
-        string s = ext[0];
-        unsigned j = 0, m = ext[0].size();
-        for (unsigned i = 1; i < ext.size(); ++i) {
-          if (ext[i].size() < m || (ext[i].size() == m && ext[i] < s)) {
-            s = ext[i];
-            m = ext[i].size();
-            j = i;
-          }
-        }
-//        std::cerr << "\"" << s << "\" chosen!\n";
-        if (a == 0)
-          return me_failed(x[p.pos].eq(home, s)) ? ES_FAILED : ES_OK;
-        if (ext.size() == 1)
-          return ES_FAILED;
-        x[p.pos].ext_remove(j);
+      const PosLevVal& p = static_cast<const PosLevVal&>(c);
+      // this->print(home, c, a, std::cerr); std::cerr << '\n';
+      const std::vector<string>& ext = x[p.pos].ext_list();
+      // std::cerr << "ext_list.size() = " << ext.size() << "\n";
+      if (ext.empty()) {
+        x[p.pos].commit(home, p.lev, p.val, Block::FIRST, a);
+        // std::cerr << a << ") Set: " << x[p.pos] << "\n";
         return ES_OK;
+      }
+      // std::cerr<< a << ") ext list = ["; for(auto x:ext) std::cerr<<"\""<<x<< "\", "; std::cerr<<"]\n";
+      if (a == 1 && ext.size() == 1)
+        return ES_FAILED;
+      string s = ext[0];
+      unsigned j = 0, m = ext[0].size();
+      for (unsigned i = 1; i < ext.size(); ++i) {
+        if (ext[i].size() > m || (ext[i].size() == m && ext[i] > s)) {
+          s = ext[i];
+          m = ext[i].size();
+          j = i;
+        }
+      }
+      if (a == 0) {
+        // std::cerr << "\"" << s << "\" chosen!\n";
+        return me_failed(x[p.pos].eq(home, s)) ? ES_FAILED : ES_OK;
+      }
+      x[p.pos].ext_remove(j);
+      if (ext.size() == 1) {
+        // std::cerr << "\"" << ext[0] << "\" chosen!\n";
+        return me_failed(x[p.pos].eq(home, ext[0])) ? ES_FAILED : ES_OK;
+      }
+      return ES_OK;
     }
 
 }}}
