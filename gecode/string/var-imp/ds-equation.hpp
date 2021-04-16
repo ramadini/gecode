0fd773bab97f64892c052a9dd4ab6ab50bc713a5
robama <amadini@cs.unibo.it>
2021-04-16 14:09:11 +0200

    (in-)equality operators

--- gecode/string/var-imp/ds-equation.hpp
+++ gecode/string/var-imp/ds-equation.hpp
@@ -13,12 +13,21 @@ namespace Gecode { namespace String {
     Position(int i, int o): idx(i), off(o) {};
     /// Check if the position is valid for dashed string \a x. If |x|=n, the valid 
     /// positions for x are {(i,j) | 0 <= i < n, 0 <= j < x[i]} U {(n,0)}.
-    forceinline bool validFor(const DashedString& x) {
+    forceinline bool 
+    validFor(const DashedString& x) {
       int n = x.size();
       return idx >= 0 && off >= 0 && 
             ((idx < n && off < x[idx].ub()) || (idx == n && off == 0));
     }
+    /// Position equality.
+    forceinline bool
+    operator==(const Position& p) { return idx == p.idx && off == p.off; }
+    /// Position inequality.
+    forceinline bool
+    operator!=(const Position& p) { return !(*this == p); }
   };
+  
+  typedef std::vector<int, std::vector<const Block&>> Matching;
   
 }}
 
@@ -27,7 +36,7 @@ namespace Gecode { namespace String {
     // FIXME: Maybe define a namespace for equate-based functions?
     
     forceinline ModEvent
-    equate_x(Space& home, DashedString& x, const DashedString& y) {
+    equate_x(Space&, DashedString&, const DashedString&) {
      
       return ME_STRING_NONE;
     }
