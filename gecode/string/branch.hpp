415f708bc4cca683b7938cec66ffcf33fc32dd68
robama <amadini@cs.unibo.it>
2021-06-08 18:56:46 +0200

    Added gecode/string/branch.hpp

---
+++ gecode/string/branch.hpp
@@ -0,0 +1,225 @@
+namespace Gecode { namespace String { namespace Branch {
+
+  forceinline Choice*
+  StringBrancher::val_llll(int pos, Gecode::String::StringView& x) const {
+    if (x.min_length() < x.max_length())
+      return new PosLevVal(*this, pos, Lev::LENGTH, Val::MIN);
+    Block& b = x[x.leftmost_unfixed_idx()];
+    if (b.lb() < b.ub())
+      return new PosLevVal(*this, pos, Lev::CARD, Val::MIN);
+    else
+      return new PosLevVal(*this, pos, Lev::BASE, Val::MIN);
+  }
+  
+  forceinline Choice*
+  StringBrancher::val_lllm(int pos, Gecode::String::StringView& x) const {
+    if (x.min_length() < x.max_length())
+      return new PosLevVal(*this, pos, Lev::LENGTH, Val::MIN);
+    Block& b = x[x.leftmost_unfixed_idx()];
+    if (b.lb() < b.ub())
+      return new PosLevVal(*this, pos, Lev::CARD, Val::MIN);
+    else
+      return new PosLevVal(*this, pos, Lev::BASE, Val::MUST_MIN);
+  }
+  
+  forceinline Choice*
+  StringBrancher::val_lslm(int pos, Gecode::String::StringView& x) const {
+    if (x.min_length() < x.max_length())
+      return new PosLevVal(*this, pos, Lev::LENGTH, Val::MIN);
+    Block& b = x[x.smallest_unfixed_idx()];
+    if (b.lb() < b.ub())
+      return new PosLevVal(*this, pos, Lev::CARD, Val::MIN);
+    else
+      return new PosLevVal(*this, pos, Lev::BASE, Val::MUST_MIN);
+  }
+  
+  forceinline void
+  StringBrancher::commit1(Space& home, StringView& x, Lev lev, Val val, int idx) {
+    // FIXME: Implement me.
+    Block& block = x[idx];
+    switch (lev) {
+      case LENGTH: {
+        if (x.min_length() == x.max_length())
+          home.fail();
+        switch (val) {
+          case MIN:
+//            x.min_length(home, x.min_length() + 1);
+            return;
+          case MAX:
+//            x.max_length(home, x.max_length() - 1);
+            return;
+          default:
+            GECODE_NEVER;
+        }
+      }
+      case CARD: {
+        if (block.lb() == block.ub())
+          home.fail();
+        switch (val) {
+          case MIN:
+             block.lb(home, block.lb()+1);
+             x.bnd_length(home, x.min_length()+1, x.max_length());
+             return;
+          case MAX:
+             block.ub(home, block.ub()-1);
+             x.bnd_length(home, x.min_length(), x.max_length()-1);
+             return;
+          default:
+            GECODE_NEVER;
+        }
+      }
+      case BASE: {
+        Gecode::Set::LUBndSet S1;
+//        block.includeBaseIn(home,S1);
+        if (S1.size() == 1)
+          home.fail();
+        int l = block.lb();
+        Gecode::Set::SetDelta d;
+        switch (val) {
+          case MIN: {
+            int m = S1.min();
+            S1.exclude(home, m, m, d);
+            break;
+          }
+          case MAX: {
+            int m = S1.max();
+            S1.exclude(home, m, m, d);
+            break;
+          }
+          case MUST_MIN: {
+//            int m;
+//            if (_MUST_CHARS.disjoint(S1))
+//              m = S1.min();
+//            else {
+//              NSIntSet t(_MUST_CHARS);
+//              t.intersect(S1);
+//              m = t.min();
+//            }
+//            S1.exclude(home, m, m, d);
+            break;
+          }
+          default:
+            GECODE_NEVER;
+        }
+        bool norm = (idx > 0 /* && x[idx-1].baseEquals(S1)*/) ||
+          (l == 1 && idx < x.size() - 1 /*&& x[idx+1].baseEquals(S1)*/);
+        if (l == 1 && !norm) {
+//          x[idx].baseUpdate(home,S1);
+          return;
+        }
+//        _blocks.insert(h, idx, DSBlock(h, S1, 1, 1));
+//        _blocks.at(idx + 1).l--;
+//        _blocks.at(idx + 1).u--;
+        if (norm)
+          x.normalize(home);
+        return;
+      }
+      default:
+        GECODE_NEVER;
+    }
+  }
+  
+  forceinline void
+  StringBrancher::commit0(Space& home, StringView& x, Lev lev, Val val, int idx) {
+    // FIXME: Implement me.
+    Block& block = x[idx];
+    switch (lev) {
+      case LENGTH: {
+        switch (val) {
+          case MIN:
+//            max_length(home, min_length());
+            return;
+          case MAX:
+//            min_length(home, max_length());
+            return;
+          default:
+            GECODE_NEVER;
+        }
+      }
+      case CARD: {
+        int k = block.ub() - block.lb();
+        switch (val) {
+          case MIN:
+            block.ub(home, block.lb());
+            x.bnd_length(home, x.min_length(), x.max_length()-k);
+            return;
+          case MAX:
+            block.lb(home, block.ub());
+            x.bnd_length(home, x.min_length()+k, x.max_length());
+            return;
+          default:
+            GECODE_NEVER;
+        }
+      }
+      case BASE: {
+        int l = block.lb();
+//        const DSIntSet& S = block.S;
+//        DSIntSet s;
+//        switch (val) {
+//          case MIN:
+//            s.init(h, S.min());
+//            break;
+//          case MAX:
+//            s.init(h, S.max());
+//            break;
+//          case MUSTMIN:
+//            if (_MUST_CHARS.disjoint(S))
+//              s.init(h, S.min());
+//            else {
+//              NSIntSet t(_MUST_CHARS);
+//              t.intersect(S);
+//              s.init(h, t.min());
+//            }
+//            break;
+//          case MUSTMAX:
+//            if (_MUST_CHARS.disjoint(S))
+//              s.init(h, S.max());
+//            else {
+//              NSIntSet t(_MUST_CHARS);
+//              t.intersect(S);
+//              s.init(h, t.max());
+//            }
+//            break;
+//          default:
+//            GECODE_NEVER;
+//        }
+//        bool norm = (idx > 0 && at(idx - 1).S == s) ||
+//          (l == 1 && idx < length() - 1 && at(idx + 1).S == s);
+//        if (l == 1 && !norm) {
+//          at(idx).S.update(h, s);
+//          return;
+//        }
+//        _blocks.insert(h, idx, DSBlock(h, s, 1, 1));
+//        _blocks.at(idx + 1).l--;
+//        _blocks.at(idx + 1).u--;
+//        if (norm)
+//          normalize(home);
+        return;
+      }
+      default:
+        GECODE_NEVER;
+    }
+  }
+  
+  forceinline void
+  StringBrancher::commit(Space& home, StringView& x, Lev l, Val v, Blc b, unsigned a) {
+    int i;
+    switch (b) {
+    case LEFTMOST:
+      i = x.leftmost_unfixed_idx();
+      break;
+    case RIGHTMOST:
+      i = x.rightmost_unfixed_idx();
+      break;
+    case SMALLEST:
+      i = x.smallest_unfixed_idx();
+      break;
+    default:
+      GECODE_NEVER;
+    }
+    a == 0 ? commit0(home, x, l, v, i) : commit1(home, x, l, v, i);  
+    assert (x.isOK());
+  }
+
+  
+}}}
