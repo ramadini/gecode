1d024ed54ec19d18b9b9c29de05d8d4b056132e2
robama <amadini@cs.unibo.it>
2021-10-27 14:14:17 +0200

    Minor.

--- gecode/string/rel-op/reverse.hpp
+++ gecode/string/rel-op/reverse.hpp
@@ -47,44 +47,43 @@ namespace Gecode { namespace String { namespace RelOp {
   Reverse<View0,View1>::propagate(Space& home, const ModEventDelta&) {
 //    std::cerr<<"\n"<<this<<"::Rev::propagate " << x0 << "  vs  " << x1 << "\n";
     refine_card(home);
-    if (x0.assigned()) {
-      if (x0.isNull()) {
-        GECODE_ME_CHECK(x1.nullify(home));
-        return home.ES_SUBSUMED(*this);
-      }
-      if (check_equate_x(x1,ReverseView(x0))) {
-        if (!x1.assigned())
-          x1.gets_rev(home, x0);
-        return home.ES_SUBSUMED(*this);
-      }
-      else
-        return ES_FAILED;
-    }
-    if (x1.assigned()) {
-      if (x1.isNull()) {
-        GECODE_ME_CHECK(x0.nullify(home));
-        return home.ES_SUBSUMED(*this);
-      }
-      if (check_equate_x(x0, ReverseView(x1))) {
-        x0.gets_rev(home, x1);
-        return home.ES_SUBSUMED(*this);
-      }
-      else
-        return ES_FAILED;
-    }
-    if (x0.contains_rev(x1)) {
-      if (x0.equals_rev(x1))
-        return ES_FIX;
-      x0.gets_rev(home, x1);
-      return ES_FIX;
-    }
-    if (x1.contains_rev(x0)) {
-      x1.gets_rev(home, x0);
-      return ES_FIX;
-    }
-    ModEvent me0;
     do {
-      me0 = x0.equate(home, ReverseView(x1));
+      if (x0.assigned()) {
+        if (x0.isNull()) {
+          GECODE_ME_CHECK(x1.nullify(home));
+          return home.ES_SUBSUMED(*this);
+        }
+        if (check_equate_x(x1,ReverseView(x0))) {
+          if (!x1.assigned())
+            x1.gets_rev(home, x0);
+          return home.ES_SUBSUMED(*this);
+        }
+        else
+          return ES_FAILED;
+      }
+      if (x1.assigned()) {
+        if (x1.isNull()) {
+          GECODE_ME_CHECK(x0.nullify(home));
+          return home.ES_SUBSUMED(*this);
+        }
+        if (check_equate_x(x0, ReverseView(x1))) {
+          x0.gets_rev(home, x1);
+          return home.ES_SUBSUMED(*this);
+        }
+        else
+          return ES_FAILED;
+      }
+      if (x0.contains_rev(x1)) {
+        if (x0.equals_rev(x1))
+          return ES_FIX;
+        x0.gets_rev(home, x1);
+        return ES_FIX;
+      }
+      if (x1.contains_rev(x0)) {
+        x1.gets_rev(home, x0);
+        return ES_FIX;
+      }
+      ModEvent me0 = x0.equate(home, ReverseView(x1));
       GECODE_ME_CHECK(me0);
       ModEvent me1 = x1.equate(home, ReverseView(x0));
       GECODE_ME_CHECK(me1);
