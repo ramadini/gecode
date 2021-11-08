2e007d8949ebba3f44777098baca083a225b99ef
robama <amadini@cs.unibo.it>
2021-11-08 14:10:41 +0100

    Minor.

--- gecode/string/int/find.hpp
+++ gecode/string/int/find.hpp
@@ -151,21 +151,21 @@ namespace Gecode { namespace String { namespace Int {
     
     // General case.
     int ll = ln;
-    //TODO: GECODE_ME_CHECK(x1.find(home, x0, l, u, occ));
+    //TODO 2: GECODE_ME_CHECK(x1.find(home, x0, l, u, occ));
     GECODE_ME_CHECK(x2.lq(home, un));
     if (occ) {
       // Can modify x and y.
       GECODE_ME_CHECK(x2.gq(home, ln));
       if (x0.assigned()) {
         if (x1.assigned()) {
-          int n = 0; // FIXME: (int) x1.val().find(x0.val()) + 1;
+          int n = find_fixed(x0,x1);
           GECODE_ME_CHECK(x2.eq(home, n));
           return home.ES_SUBSUMED(*this);
         }
         if (x2.assigned() && x1.min_length() == x1.max_length()) {
           int n = x2.val(), m = x0.min_length();
           assert (n > 0);
-          //FIXME:  GECODE_ME_CHECK(x1.eq(home, x0.val().substr(n-1, m)));
+          //TODO 1:  GECODE_ME_CHECK(x1.eq(home, x0.val().substr(n-1, m)));
           return home.ES_SUBSUMED(*this); 
         }
       }
